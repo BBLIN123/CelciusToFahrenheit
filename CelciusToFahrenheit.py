cab522d1cc9e312b3d03648cf585525cc935815c
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2023-08-06 09:22:18 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -41,7 +41,8 @@ def main():
             ),
             pdk.Layer(
                'HexagonLayer',
-               data=pd.DataFrame("lat": [-27.4402576,-27.634058,-27.4797838],
+               data=pd.DataFrame(
+                "lat": [-27.4402576,-27.634058,-27.4797838],
                 "lon":[153.0693872,152.9693843,153.0141015]),
                get_position='[lon, lat]',
                radius=50,
