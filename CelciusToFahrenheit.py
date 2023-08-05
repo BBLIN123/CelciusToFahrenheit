f5800c7ea7932a9ce501da8f063c0ea877f6ea31
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2023-08-05 17:35:53 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -39,6 +39,21 @@ def main():
                pickable=True,
                extruded=True,
             ),
+            layers=[
+            pdk.Layer(
+               'HexagonLayer',
+               data=pd.DataFrame("lat": [-27.4402576
+-27.634058
+-27.4797838], "lon":[153.0693872
+152.9693843
+153.0141015]),
+               get_position='[lon, lat]',
+               radius=50,
+               elevation_scale=4,
+               elevation_range=[0, 1000],
+               pickable=True,
+               extruded=True,
+            ),
             pdk.Layer(
                 'ScatterplotLayer',
                 data=chart_data,
