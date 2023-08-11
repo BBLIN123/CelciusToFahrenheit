6d1296b955388d365efd7751a40e676f51f9bc3b
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2023-08-11 20:01:45 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -92,7 +92,7 @@ def main():
                'HexagonLayer',
                data=manual_data,
                get_position='[lon, lat]',
-               radius=40,
+               radius=100,
                elevation_scale=5,
                elevation_range=[10,200],
                pickable=True,
