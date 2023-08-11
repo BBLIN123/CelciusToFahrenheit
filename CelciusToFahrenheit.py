68c9b5c929d11edfafa57e8474587966e809120d
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2023-08-11 19:18:58 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -94,7 +94,7 @@ def main():
                get_position='[lon, lat]',
                radius=50,
                elevation_scale=8,
-               elevation_range=[0,1000],
+               elevation_range=[1000,1000],
                pickable=True,
                extruded=True,
             ),
