50478528b6e1634c5a81df7550b9ba63fc37d913
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2024-04-01 15:17:57 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -33,7 +33,7 @@ def main():
     '''
  
     chart_data = pd.DataFrame(
-       np.random.randn(10, 3) / [50, 50, 1] * [1, 1, 30] + [-27.634, 152.969, 105],
+       np.random.randn(100, 3) / [50, 50, 1] * [1, 1, 30] + [-27.634, 152.969, 105],
        columns=['lat', 'lon', 'elevation'])
     manual_data = pd.DataFrame(
         [
