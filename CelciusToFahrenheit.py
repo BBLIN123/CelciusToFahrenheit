41dad026abc8390d22de66579edaeb7049def3fd
BBLIN123 <124500258+BBLIN123@users.noreply.github.com>
2024-04-04 00:26:23 +1000

    Update CelciusToFahrenheit.py

--- CelciusToFahrenheit.py
+++ CelciusToFahrenheit.py
@@ -113,7 +113,7 @@ def main():
     
     
     col1, col2, col3 = st.columns(3)
-    col1.metric("Temperature", "70 °F", "1.2 °F")
+    col1.metric("Temperature", {celsius_temp:.2f}°C, "1.2 °F")
     col2.metric("Wind", "9 mph", "-8%")
     col3.metric("Humidity", "86%", "4%")
     
